949d30d283fa8504583f1065d6f2bc2c0e5aa5ac
Leon Philip Sanders <leonphilip@Luftbuch-2.local>
2015-08-19 17:45:05 +0200

    accordion done

--- js/cp2015.js
+++ js/cp2015.js
@@ -23,7 +23,22 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
-//Accodion list toggle
-$(function() {
-    $( "#accordion" ).accordion({autoHeight: false, heightStyle: "content"});
-  });
+/*Show more/less*/
+// Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+$('.read-more-content').addClass('hide')
+$('.read-more-show, .read-more-hide').removeClass('hide')
+
+// Set up the toggle effect:
+$('.read-more-show').on('click', function(e) {
+  $(this).next('.read-more-content').removeClass('hide');
+  $(this).addClass('hide');
+  e.preventDefault();
+});
+
+// Changes contributed by @diego-rzg
+$('.read-more-hide').on('click', function(e) {
+  var p = $(this).parent('.read-more-content');
+  p.addClass('hide');
+  p.prev('.read-more-show').removeClass('hide'); // Hide only the preceding "Read More"
+  e.preventDefault();
+});
