41a8b04806406c28591999254b835630e0c7aa5e
Leon Philip Sanders <leonphilip@Luftbuch-2.local>
2015-08-24 23:49:18 +0200

    Finally

--- js/cp2015.js
+++ js/cp2015.js
@@ -35,10 +35,82 @@ $('.read-more-show').on('click', function(e) {
   e.preventDefault();
 });
 
-// Changes contributed by @diego-rzg
 $('.read-more-hide').on('click', function(e) {
   var p = $(this).parent('.read-more-content');
   p.addClass('hide');
   p.prev('.read-more-show').removeClass('hide'); // Hide only the preceding "Read More"
   e.preventDefault();
 });
+
+
+//Calendar by fullcalendar.io
+$(document).ready(function() {
+        
+        $('#calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            defaultDate: '2015-02-12',
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            events: [
+                {
+                    title: 'All Day Event',
+                    start: '2015-02-01'
+                },
+                {
+                    title: 'Long Event',
+                    start: '2015-02-07',
+                    end: '2015-02-10'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2015-02-09T16:00:00'
+                },
+                {
+                    id: 999,
+                    title: 'Repeating Event',
+                    start: '2015-02-16T16:00:00'
+                },
+                {
+                    title: 'Conference',
+                    start: '2015-02-11',
+                    end: '2015-02-13'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2015-02-12T10:30:00',
+                    end: '2015-02-12T12:30:00'
+                },
+                {
+                    title: 'Lunch',
+                    start: '2015-02-12T12:00:00'
+                },
+                {
+                    title: 'Meeting',
+                    start: '2015-02-12T14:30:00'
+                },
+                {
+                    title: 'Happy Hour',
+                    start: '2015-02-12T17:30:00'
+                },
+                {
+                    title: 'Dinner',
+                    start: '2015-02-12T20:00:00'
+                },
+                {
+                    title: 'Birthday Party',
+                    start: '2015-02-13T07:00:00'
+                },
+                {
+                    title: 'Click for Google',
+                    url: 'http://google.com/',
+                    start: '2015-02-28'
+                }
+            ]
+        });
+        
+    });
